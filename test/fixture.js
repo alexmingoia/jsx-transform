befad0e5cc204fbaa896f42e54a4405eb55bd024
Markus Kobler <markus@distinctive.co>
2015-04-18 10:47:54 +0100

    dont pass Component(null) when no children or attributes

--- test/fixture.js
+++ test/fixture.js
@@ -5,7 +5,9 @@ module.exports = function () {
   var profile = Component(null, [x = 2])
   var h1 = DOM('h1', {class: "header"}, ["Hello ", firstName + " " + lastName]);
 
-  if (x < 2) {
+  if (x < 1) {
+    return DOM('div')
+  } else if (x < 2) {
     return DOM('h1', null, ["One is less than two"]);
   } else {
     return DOM('div', {class: "title"}, [DOM('h1', null, ["elements can be nested"])]);
--- test/fixture.jsx
+++ test/fixture.jsx
@@ -5,7 +5,9 @@ module.exports = function () {
   var profile = <Component>{x = 2}</Component>
   var h1 = <h1 class="header">Hello {firstName + " " + lastName}</h1>;
 
-  if (x < 2) {
+  if (x < 1) {
+    return <div></div>
+  } else if (x < 2) {
     return <h1>One is less than two</h1>;
   } else {
     return <div class="title"><h1>elements can be nested</h1></div>;
